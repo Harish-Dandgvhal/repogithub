99efc9ad3b928c7e7b7b7e54f2133fa5d0b230e3
D6_63275_Harish <hdandagwhal@gmail.com>
2022-06-23 19:16:39 +0530

    odd fun added

--- myfun.js
+++ myfun.js
@@ -8,3 +8,14 @@ function isEven(num) {
 
 isEven(10)
 isEven(11)
+
+function isOdd(num) {
+  if (num % 2 == 0) {
+    console.log(`number is not odd${num}`)
+  } else {
+    console.log(`number is odd${num}`)
+  }
+}
+
+isOdd(12)
+isOdd(11)
